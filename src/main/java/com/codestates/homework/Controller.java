377c10520d64738b5965caad953815a4f6a4e862
wkqltm95 <wkqltm95@gmail.com>
2022-10-21 14:46:43 +0900

    과제 요구사항 구현

--- src/main/java/com/codestates/homework/Controller.java
+++ src/main/java/com/codestates/homework/Controller.java
@@ -1,7 +1,14 @@
 package com.codestates.homework;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class Controller {
+    @GetMapping
+    public String helloWorld(){
+        return "To-do Application !";
+    }
 }
